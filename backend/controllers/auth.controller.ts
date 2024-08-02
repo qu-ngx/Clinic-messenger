34e03c2e2f9999bcde9750a0db8e93d9fe477cc3
qu-ngx <quang13012005@gmail.com>
2024-08-02 08:13:47 -0700

    added jwt + fixed prisma import path

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import prisma from "../db/prisma.js"
 import bcryptjs from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
 
 export const signup = async (req:Request, res:Response) => {
     try {
-        const { fullname, username, password, confirmPassword, gender } = req.body;
+        const { fullName, username, password, confirmPassword, gender } = req.body;
         
-        if (!fullname || !username || !password || !confirmPassword || !gender) {
+        if (!fullName || !username || !password || !confirmPassword || !gender) {
             return res.status(400).json({error: "Please correct all the fields"});
         }
 
@@ -24,13 +25,38 @@ export const signup = async (req:Request, res:Response) => {
         const salt = await bcryptjs.genSalt(13); // Roll the encryption 13 times whoo whoo
         const hashedPassword = await bcryptjs.hash(password, salt); 
 
+        // Use Placeholder API to generate avatar
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
+        // Create a new user request with prisma (Pass in the body of )
+        const newUser = await prisma.user.create({
+            data: {
+                fullName,
+                username,
+                password: hashedPassword,
+                gender,
+                profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+            }
+        });
 
+        // Generate JWT Token for user signup
+        if (newUser) {
+            generateToken(newUser.id, res);
 
-    } catch (error) {
+            res.status(200).json({
+                id: newUser.id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({error: "Invalid user data"});
+        } 
 
+    } catch (error: any) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
     }
 };
 export const login = async (req:Request, res:Response) => {};
--- backend/db/prisma.ts
+++ backend/db/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client/extension";
+// Import the right path for prisma client
+import { PrismaClient } from "../../node_modules/.prisma/client/index.js";
 
 const prisma = new PrismaClient();
 
