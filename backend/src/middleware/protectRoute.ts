f464f29667d94af4d809224c10f1f2cbe0b0139d
qu-ngx <quang13012005@gmail.com>
2024-08-02 15:00:03 -0700

    added 500 code handle for me endpoint

--- backend/src/middleware/protectRoute.ts
+++ backend/src/middleware/protectRoute.ts
@@ -34,7 +34,8 @@ const protectRoute = async (req: Request, res: Response, next: NextFunction) => 
         }
 
         // Find the user in the db with the token verified
-        const user = await prisma.user.findUnique({ where: { id: decoded.userId }, select: {id: true, username: true, fullname: true, profilePic: true}});
+        const user = await prisma.user.findUnique({ where: { id: decoded.userId }, 
+            select: {id: true, username: true, fullname: true, profilePic: true}});
 
         if (!user) {
             return res.status(404).json({ error: "User not found"});
@@ -43,9 +44,9 @@ const protectRoute = async (req: Request, res: Response, next: NextFunction) => 
         req.user = user;
 
         next();
-        
-    } catch (error) {
-        
+    } catch (error: any) {
+        console.log("Error in protectRoute middleware", error.message);
+        res.status(500).json({error: "Internal Server Error"});
     }
 }
 
