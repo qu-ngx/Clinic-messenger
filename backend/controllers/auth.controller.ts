a78eb79ffe421141bca77d828362769978e72961
qu-ngx <quang13012005@gmail.com>
2024-08-02 10:42:50 -0700

    added getMe route

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -98,3 +98,12 @@ export const logout = async (req:Request, res:Response) => {
 		res.status(500).json({ error: "Internal Server Error" });
 	}
 };
+
+export const getMe = async(req:Request, res:Response) => {
+    try {
+        const user = await prisma.user.findUnique({where:{id.req.user.id}});
+    } catch (error: any) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+    }
+}
