b2579a0faf2f4f2a0017cab26ef954c382792424
qu-ngx <quang13012005@gmail.com>
2024-08-01 19:33:58 -0700

    finished prisma schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import authRoutes from "../routes/auth.route.js";
 import messageRoutes from "../routes/message.route.js";
 
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 // Setting up routes
