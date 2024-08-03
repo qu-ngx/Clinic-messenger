2f8c5cd90b5a7e87665cdcdf5d428b1bf48f22c3
qu-ngx <quang13012005@gmail.com>
2024-08-03 00:40:33 -0700

    added frontend components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+// import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+// import { useAuthContext } from "./context/AuthContext";
+// import { Toaster } from "react-hot-toast";
+
 function App() {
-  return (
-    <>
-  
-    </>
-  )
+	// const { authUser, isLoading } = useAuthContext();
+
+	// if (isLoading) return null;
+
+	return (
+		<div className='p-4 h-screen flex items-center justify-center'>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/' element={<SignUp />} />
+				<Route path='/' element={<Login />} />
+				{/* <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/signup' element={!authUser ? <SignUp /> : <Navigate to={"/"} />} />
+				<Route path='/login' element={!authUser ? <Login /> : <Navigate to={"/"} />} /> */}
+			</Routes>
+			{/* <Toaster /> */}
+		</div>
+	);
 }
 
-export default App
+export default App;
