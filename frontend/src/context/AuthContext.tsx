18171242e49686519231865e3f8750572af71dc2
qu-ngx <quang13012005@gmail.com>
2024-08-04 00:07:53 -0700

    added global special type

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,63 +1,63 @@
-import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useState } from "react";
+import { Dispatch, ReactNode, SetStateAction, createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 type AuthUserType = {
-    id: string;
-    fullname: string;
-    email: string;
-    profilePic: string;
-    gender: string
-}
+	id: string;
+	fullname: string;
+	email: string;
+	profilePic: string;
+	gender: string;
+};
 
-//Declare init states, generics for each type of the state
 const AuthContext = createContext<{
-    authUser: AuthUserType | null;
-    setAuthUser: Dispatch<SetStateAction<AuthUserType | null>>;
-    isLoading: boolean;
+	authUser: AuthUserType | null;
+	setAuthUser: Dispatch<SetStateAction<AuthUserType | null>>;
+	isLoading: boolean;
 }>({
-    authUser: null,
-    setAuthUser: () => {},
-    isLoading: true,
-
+	authUser: null,
+	setAuthUser: () => {},
+	isLoading: true,
 });
 
-// Create some hooks
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuthContext = () => {
-    return useContext(AuthContext);
-}
+	return useContext(AuthContext);
+};
 
-// Wrap the whole children (App) in the Context provider. 
-export const AuthContextProvider = ({children}:{children:ReactNode}) => {
-    const [authUser, setAuthUser] = useState<AuthUserType | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
-    // Logic to check user is available or not 
-    useEffect(() => {
-        const fetchAuthUser = async () => {
-            try {
-                const res = await fetch("/api/auth/me")
-                const data = await res.json();
-                if (!res.ok) {
-                    throw new Error(data.message);
-                }
-                setAuthUser(data);
-            } catch (error: any) {
-               console.error(error);
-            } finally {
-                setIsLoading(false);
-            }
-        }
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+	const [authUser, setAuthUser] = useState<AuthUserType | null>(null);
+	const [isLoading, setIsLoading] = useState(true);
 
-        fetchAuthUser();
-    }, []);
-    return (
-        <AuthContext.Provider 
-            value={{
-                authUser,
-                isLoading,
-                setAuthUser
-            }}
-        
-        >
-            {children}
-        </AuthContext.Provider>
-    )
-}
+	// logic will go here
+	useEffect(() => {
+		const fetchAuthUser = async () => {
+			try {
+				const res = await fetch("/api/auth/me");
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error);
+				}
+				setAuthUser(data);
+			} catch (error: any) {
+				console.error(error);
+				toast.error(error.message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchAuthUser();
+	}, []);
+
+	return (
+		<AuthContext.Provider
+			value={{
+				authUser,
+				isLoading,
+				setAuthUser,
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
+};
